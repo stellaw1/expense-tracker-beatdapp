0e70ab1584f49c2bce58997c80ed935512973c0a
stellaw1 <stella01wang@gmail.com>
2020-04-25 13:13:58 -0700

    added error input checking

--- data.js
+++ data.js
@@ -3,26 +3,50 @@ var expArray = new Array();
 const expList = document.querySelector("#expenses-list");
 var nameDir = "des";
 var costDir = "des";
+var catDir = "des";
 
 /**
  * Called on button click. Sends textbox input to local database to be stored. 
  */
 function addItem() {
+
+    //grab values from text box input from html
     var expName = document.getElementById("addName").value;
     var expCost = document.getElementById("cost").value;
     var expCategory = document.getElementById("category").value;
 
+    //check expName input is unique
+    for (var i = 0; i < expArray.length; i++) {
+        if (expName == expArray[i]) {
+            alert("Entered expense name is not unique. Please enter a name that is not already in list. ");
+            return;
+        }
+    }
+
+    //check expCost is not empty
+    if (expCost == "") {
+        alert("Please enter expense cost. ");
+        return;
+    }
+
+    //check a category is selected for expCategory
+    if (expCategory == "Select a category") {
+        alert("Please select a category. ");
+        return;
+    }
+
+    //add expense name to global array
+    expArray.push(expName);
+
+    //parse expense cost and category as JSON obj
     var expValue = { "cost": "", "category": "" };
     expValue.cost = expCost;
     expValue.category = expCategory;
 
-    console.log(expValue);
-
-    expArray.push(expName);
-    console.log(expName);
-
+    //push item to database
     localStorage.setItem(expName, JSON.stringify(expValue));
 
+    //update table of expenses
     displayData();
 }
 
@@ -30,49 +54,72 @@ function addItem() {
  * Displays expenses that are currently in the database
  */
 function displayData() {
+    
+    //clear current table to sync expenses according to database
     clearTable();
 
+    //use expArray as keys to grab items from database
     for (var i = 0; i < expArray.length; i++) {
+        //parse item value from database into JSON obj
         var exp = JSON.parse(localStorage.getItem(expArray[i]));
     
+        //create html elements - 1 row and 3 cells
         let tr = document.createElement('tr');
         let name = document.createElement('td');
         let cost = document.createElement('td');
         let category = document.createElement('td');
 
+        //set html table cells according obj from database 
         name.textContent = expArray[i];
         cost.textContent = exp.cost;
         category.textContent = exp.category;
 
+        //combine table cells into 1 row
         tr.appendChild(name);
         tr.appendChild(cost);
         tr.appendChild(category);
 
-        console.log(expArray[i]);
+        //add row to expense list table
         expList.appendChild(tr);
     }
 }
 
+/**
+ * Clears entire expense table to prepare for updating table according to database
+ */
 function clearTable() {
     while(expList.hasChildNodes())
         expList.removeChild(expList.firstChild);
-
 }
 
 /**
  * Deletes item from local database given its key value
  */
 function deleteItem() {
+    //grab input value from html 
     var expName = document.getElementById("deleteName").value;
 
+    //check entered input expense name exists in database
+    for (var i = 0; i < expArray.length; i++) {
+        if (expName == expArray[i]) {
+            break;
+        } else if (i == expArray.length - 1 && expName != expArray[i]) {
+            alert("Please enter an expense name that exists in current list. ");
+            return;
+        }
+    }
+
+    //remove item from database
     localStorage.removeItem(expName);
 
+    //remove expense from expArray
     var index = expArray.indexOf(expName);
     while (index > -1) {
         expArray.splice(index, 1);
         index = expArray.indexOf(expName);
     }
 
+    //update expense table 
     displayData();
 }    
 
@@ -101,20 +148,19 @@ function sortTableByName() {
         nameDir = "des";
     }
 
-    /* Make a loop that will continue until no switching has been done: */
+    // loop that will continue until entire table is sorted according to costDir
     while (switching) {
         switching = false;
         rows = table.rows;
 
-        /* Loop through all table rows */
         for (i = 0; i < (rows.length - 1); i++) {
-            // Start by saying there should be no switching:
+            //reset shouldSwitch marker
             shouldSwitch = false;
-            /* Get two elements to compare, one from current row and one from the next: */
+            // Get two elements to compare, one from current row and one from the next
             x = rows[i].getElementsByTagName("td")[col];
             y = rows[i + 1].getElementsByTagName("td")[col];
 
-            /* Check if the two rows should switch place, based on the direction, asc or desc: */
+            // Check if the two rows should switch place, based on the direction, asc or desc
             if (nameDir == "asc") {
                 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
                     // If so, mark as a switch and break the loop:
@@ -130,7 +176,7 @@ function sortTableByName() {
             }
         }
         if (shouldSwitch) {
-            /* If a switch has been marked, make the switch and mark that a switch has been done: */
+            // switch x and y if shouldSwitch is marked true
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
         } 
@@ -141,8 +187,8 @@ function sortTableByName() {
  * Function that sorts table by Cost. Sorting order is opposite to costDir
  */
 function sortTableByCost() {
-    console.log("sort by cost");
 
+    //declare helper variables
     var table = document.getElementById("expenses-list");
     var col = 1;
     var rows, switching, i, x, y, shouldSwitch;
@@ -156,20 +202,19 @@ function sortTableByCost() {
         costDir = "des";
     }
 
-    /* Make a loop that will continue until no switching has been done: */
+    // loop that will continue until entire table is sorted according to costDir
     while (switching) {
         switching = false;
         rows = table.rows;
 
-        /* Loop through all table rows */
         for (i = 0; i < (rows.length - 1); i++) {
-            // Start by saying there should be no switching:
+            // reset shouldSwitch marker
             shouldSwitch = false;
-            /* Get two elements to compare, one from current row and one from the next: */
+            // Get two elements to compare, one from current row and one from the next
             x = rows[i].getElementsByTagName("td")[col];
             y = rows[i + 1].getElementsByTagName("td")[col];
 
-            /* Check if the two rows should switch place, based on the direction, asc or desc: */
+            // Check if the two rows should switch place, based on costDir
             if (costDir == "asc") {
                 if (Number(x.innerHTML) > Number(y.innerHTML)) {
                     // If so, mark as a switch and break the loop:
@@ -185,7 +230,7 @@ function sortTableByCost() {
             }
         }
         if (shouldSwitch) {
-            /* If a switch has been marked, make the switch and mark that a switch has been done: */
+            // switch x and y if shouldSwitch is marked true
             rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
             switching = true;
         } 
@@ -193,65 +238,54 @@ function sortTableByCost() {
 }
 
 /**
- * Function that sorts table by Name or Cost depending on its input
- * 
- * parameter: integer representing type to sort by
+ * Function that sorts table by Name. Sorting order is opposite to nameDir
  */
-function sortTable(c) {
+function sortTableByCategory() {
+    
     var table = document.getElementById("expenses-list");
-    var rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    var col = 2;
+    var rows, switching, i, x, y, shouldSwitch;
 
     switching = true;
-    // Set the sorting direction to ascending:
-    dir = "asc";
-    /* Make a loop that will continue until
-    no switching has been done: */
-    while (switching) {
-        // Start by saying: no switching is done:
-        switching = false;
-        rows = table.rows;
-        /* Loop through all table rows (except the
-        first, which contains table headers): */
-        for (i = 0; i < (rows.length - 1); i++) {
-        // Start by saying there should be no switching:
-        shouldSwitch = false;
-        /* Get the two elements you want to compare,
-        one from current row and one from the next: */
-        x = rows[i].getElementsByTagName("td")[c];
-        y = rows[i + 1].getElementsByTagName("td")[c];
-        /* Check if the two rows should switch place,
-        based on the direction, asc or desc: */
-        if (dir == "asc") {
-            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-            }
-        } else if (dir == "desc") {
-            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-            // If so, mark as a switch and break the loop:
-            shouldSwitch = true;
-            break;
-            }
-        }
-        }
-        if (shouldSwitch) {
-        /* If a switch has been marked, make the switch
-        and mark that a switch has been done: */
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-        // Each time a switch is done, increase this count by 1:
-        switchcount ++;
-        } else {
-        /* If no switching has been done AND the direction is "asc",
-        set the direction to "desc" and run the while loop again. */
-        if (switchcount == 0 && dir == "asc") {
-            dir = "desc";
-            switching = true;
-        }
-        }
+
+    // set nameDir to the opposite of its current order
+    if (catDir == "des") {
+        catDir = "asc";
+    } else if (catDir == "asc") {
+        catDir = "des";
     }
 
-}
+    // loop that will continue until entire table is sorted according to costDir
+    while (switching) {
+        switching = false;
+        rows = table.rows;
 
-clearDatabase();
+        for (i = 0; i < (rows.length - 1); i++) {
+            //reset shouldSwitch marker
+            shouldSwitch = false;
+            // Get two elements to compare, one from current row and one from the next
+            x = rows[i].getElementsByTagName("td")[col];
+            y = rows[i + 1].getElementsByTagName("td")[col];
+
+            // Check if the two rows should switch place, based on the direction, asc or desc
+            if (catDir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (catDir == "des") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            // switch x and y if shouldSwitch is marked true
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        } 
+    }
+}
